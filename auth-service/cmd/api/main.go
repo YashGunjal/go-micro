9eef14da9a5a2164cb8ce70b9f8cb77ab56236a9
Yash Gunjal <yashgunjal98@gmail.com>
2024-05-09 19:28:48 -0400

    auth service setup

---
+++ auth-service/cmd/api/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	models data.Models
+}
+
+func main() {
+	fmt.Println("Starting Authentication service")
+
+	// connect to DB
+
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
